dc6f096c7f9a0997dec180ab91cfeb51e99f33b2
ryan6073 <zhujs5521@mails.jlu.edu.cn>
2023-10-08 13:15:36 +0800

    银行家算法

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          enter my banker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/banker1">Banker</Link>
       </header>
     </div>
   );
